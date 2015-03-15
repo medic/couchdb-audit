3991bdf63742d4dfe6101a27f59015c41456584b
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2015-03-16 09:56:24 +1300

    Deprecated withNode and renamed to withFelix

--- couchdb-audit/node.js
+++ couchdb-audit/node.js
@@ -1,18 +1,25 @@
 var log = require('./log');
 
 module.exports = {
+
   /**
    * Initialise the audit logger.
    *
-   * @name withNode(felix, name)
+   * @name withFelix(felix, name)
    * @param {Object} felix The felix instance to use to persist.
    * @param {String|Function(err, name)} name Either the authors name, 
    *    or a function to retrieve the name.
    * @api public
    */
-  withNode: function(felix, name) {
+  withFelix: function(felix, name) {
     var nameFn = (typeof name === 'string') ? 
       function(callback) { callback(null, name); } : name;
     return log.init(felix.name, felix.client, felix, nameFn);
   }
+
 };
+
+/**
+ * @deprecated Use withFelix instead.
+ */
+module.exports.withNode = module.exports.withFelix;
